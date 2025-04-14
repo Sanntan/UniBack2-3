aed4115dc677962e6117fb0f26e9a5d1fb8a3bc1
Sanntan <gommaftegey04@mail.ru>
2025-04-14 19:28:05 +0300

    save

--- scripts/import_articles_from_csv.py
+++ scripts/import_articles_from_csv.py
@@ -12,7 +12,7 @@ from database.models import Article
 from sqlalchemy.exc import SQLAlchemyError
 
 # Путь к файлу CSV
-CSV_PATH = "TestData/articles_and_authors.csv"
+CSV_PATH = "TestData/articles_3800.csv"
 
 
 def import_articles():
